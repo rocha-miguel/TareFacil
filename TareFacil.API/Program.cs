779c97cbfad77bca71080165bb431141be4d3a43
rocha-miguel <miguelrocha1818@gmail.com>
2026-04-18 10:05:44 -0300

    Adiciona Scalar, Swagger e CORS para frontend Angular
    Inclui suporte ao Scalar e Swagger para documentação da API, configura CORS permitindo requisições do Angular (localhost:4200), adiciona dependências necessárias e ajusta o launchSettings para abrir na rota "scalar".

--- TareFacil.API/Program.cs
+++ TareFacil.API/Program.cs
@@ -1,17 +1,41 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var allowedOrigins = builder.Configuration
+    .GetSection("Cors:AllowedOrigins")
+    .Get<string[]>();
+
+builder.Services.AddCors(options => {
+    options.AddPolicy("AngularApp", policy => {
+        policy.WithOrigins(allowedOrigins!)
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment()) {
     app.MapOpenApi();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapScalarApiReference(s => s.WithTheme(ScalarTheme.DeepSpace));
+
+app.UseCors("AngularApp");
 
 app.UseAuthorization();
 
